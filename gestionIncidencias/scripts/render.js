6dbd7aa4f11b7d0fb44a5e7973036b725291304d
didacfrndz <fernandezbarcodidac@fpllefia.com>
2025-10-06 20:52:21 +0200

    commit

---
+++ gestionIncidencias/scripts/render.js
@@ -0,0 +1,9 @@
+// Obtenir referències dels elements DOM amb querySelector
+
+const estatSelect = document.querySelector('#estat');
+const prioritatSelect = document.querySelector('#prioritat');
+const filtroBtn = document.querySelector('#filtro');
+const btnNuevaIncidencia = document.querySelector('#btnNuevaIncidencia');
+const tablaIncidencias = document.querySelector('table.table-striped tbody');
+const statsSection = document.querySelector('#stats');
+const modalIncidencia = document.querySelector('#modalIncidencia');
