0f433897b7d1e939983712eddd47d0817676eabb
didacfrndz <fernandezbarcodidac@fpllefia.com>
2025-10-13 20:52:42 +0200

    .

--- dia3/index.js
+++ dia3/index.js
@@ -16,7 +16,16 @@ console.log(incidencies);
 //paso de objecte a text i de text a objecte
 const textoObj = JSON.stringify(incidencies);
 console.log(textoObj);
+
+//guardamos en el localStorage
+localStorage.setItem("bd", textoObj);
+
+const bdObject = localStorage.getItem("bd");
+console.log(bdObject);
+
 //paso de text a objecte
 const objTexto = JSON.parse(textoObj);
 console.log(objTexto);
+
+
 
